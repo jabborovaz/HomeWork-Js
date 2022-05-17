4434fc86ba4139e29ea8722fa83e4825de362e0c
jabborovaz <greatman-98@mail.ru>
2022-05-17 15:53:17 +0500

    add task 1 of Lesson 3

---
+++ Lesson 3_Homework/task1.js
@@ -0,0 +1,11 @@
+let film = {
+    name: "Titanic",
+    year: "1997",
+    ganre: "Drama",
+    actor: "DiCaprio",
+   rate: {
+       kinopoisk: "9.6",
+       fees: "Most grossing film!"
+   }
+};
+console.log(film);
