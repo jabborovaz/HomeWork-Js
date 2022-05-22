20fdd2310f960c5e2494d6fc747f78c6ee7636f3
jabborovaz <greatman-98@mail.ru>
2022-05-22 07:52:18 +0500

    Lesson 4 homeworks

---
+++ Lesson 4_Homework/task.js
@@ -0,0 +1,7 @@
+
+   const mass = [1,2,3,4,5,6];
+   const massWithInitial = mass.reduce((previousValue, currentValue) => previousValue + currentValue);
+   console.log(massWithInitial);
+
+
+  
---
+++ Lesson 4_Homework/task3.js
@@ -0,0 +1,2 @@
+var max = [1,2,3,4,5];
+console.log(Math.max.apply(null, max));
